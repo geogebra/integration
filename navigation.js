422cf1d58fa760ef21ff69cd47b8adf0a7d13590
AliciaHof <alicia.hofstaetter@geogebra.org>
2020-06-15 23:00:49 +0200

    add frameless param

--- navigation.js
+++ navigation.js
@@ -68,8 +68,28 @@ function addDrawer(){
 
 function loadNav(){
 	insertStyle("https://fonts.googleapis.com/icon?family=Material+Icons");
-	addGithubLink();
-	addDrawer();
+	
+	var frameless = false;
+	var getVars = [];
+	var locvartemp = ( window.location.href.indexOf( "?" ) + 1 ) ? window.location.href.substr( window.location.href.indexOf( "?" ) + 1 ) : "";
+	locvartemp = unescape(locvartemp);
+	var lvTempVar = locvartemp.split( "=" );
+	getVars[ unescape( lvTempVar[0] ) ] = unescape( lvTempVar[1] );
+	try {
+		console.log(locvartemp);
+		eval(locvartemp);
+		}
+		catch (err) { 
+			console.log(err);
+			eval(lvTempVar[0] + "='" + lvTempVar[1] + "'");
+		} 
+
+	if(frameless == false ){
+		addGithubLink();
+		addDrawer();
+	} else {
+		document.getElementById("contentBox").classList.add('contentBoxSmall');
+	}
 }
 
 function insertStyle(url){
