5802d08da88bc28606cda3bbb073ea339ecb3d89
Zbynek Konecny <zbynek@geogebra.at>
2024-04-16 15:06:47 +0200

    Add material embedding page

--- navigation.js
+++ navigation.js
@@ -36,7 +36,7 @@ function addDrawer(){
 	'<li><a href="https://geogebra.github.io/docs/reference/en/GeoGebra_Apps_Embedding/" target="_blank">Math Apps Embedding&nbsp;&nbsp;<span class="material-icons inline">arrow_downward</span></a></li>'+
 	'<li><a href="https://geogebra.github.io/docs/reference/en/GeoGebra_App_Parameters/" target="_blank">App Parameters&nbsp;&nbsp;<span class="material-icons inline">arrow_downward</span></a></li>'+
 	'<li><a href="https://geogebra.github.io/docs/reference/en/GeoGebra_Apps_API/" target="_blank">GeoGebra Apps API&nbsp;&nbsp;<span class="material-icons inline">arrow_downward</span></a></li>'+
-	'<li><a href="https://wiki.geogebra.org/en/Reference:Material_Embedding_(Iframe)" target="_blank">Iframe Embedding&nbsp;&nbsp;<span class="material-icons inline">arrow_downward</span></a></li>';
+	'<li><a href="https://geogebra.github.io/docs/reference/en/Material_Embedding_(Iframe)" target="_blank">Iframe Embedding&nbsp;&nbsp;<span class="material-icons inline">arrow_downward</span></a></li>';
 
 
 
